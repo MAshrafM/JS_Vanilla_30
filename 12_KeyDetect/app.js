13434e57a9d5d3dde0abbdb96a4bdcdf0ea6944c
MAshraf <ee.mashraf@gmail.com>
2017-03-11 19:16:40 +0200

    app

--- 12_KeyDetect/app.js
+++ 12_KeyDetect/app.js
@@ -1,4 +1,15 @@
 "use strict";
 (function(){
-  
+  const pressed = [];
+  const secretCode = 'sonic';
+  const textpad = document.getElementById("textpad");
+  window.addEventListener('keyup', (e) => {
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+      textpad.innerHTML = "Secret Cracked!"
+      cornify_add();
+    }
+    textpad.innerHTML += e.key;
+  });
 })();
