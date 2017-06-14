dc30f43088ae2835084f77d21e8394d13e76b582
MAshraf <ee.mashraf@gmail.com>
2017-06-14 06:08:19 +0300

    app

--- 21_GeoLoc/app.js
+++ 21_GeoLoc/app.js
@@ -1,4 +1,14 @@
 "use strict";
 (function(){
+  const arrow = document.querySelector('.arrow');
+  const speed = document.querySelector('.speed-value');
+
+  navigator.geolocation.watchPosition((data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  }, (err) => {
+    console.error(err);
+  });
   
 })();
