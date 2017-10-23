e7b179d5a001bc2fdd6f212ea02bfa11995bda7b
MAshraf <ee.mashraf@gmail.com>
2017-10-23 17:01:06 +0200

    animate

--- 25_CaptureProp/app.js
+++ 25_CaptureProp/app.js
@@ -1,4 +1,9 @@
 "use strict";
 (function(){
-
+  const c1 = document.getElementById('subset')
+  const c2 = document.getElementById('goo')
+  let click = document.getElementById('act').addEventListener('click', function() {
+    c1.style.display = "block"; 
+    c2.style.display = "block";     
+  })
 }());
