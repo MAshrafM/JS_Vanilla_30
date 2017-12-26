ef264e8487104f39d857cea36dd97395ff091e0b
MAshraf <ee.mashraf@gmail.com>
2017-12-26 19:40:08 +0200

    dynamic html

--- 27_ClickDrag/app.js
+++ 27_ClickDrag/app.js
@@ -1,4 +1,24 @@
 "use strict";
+
 (function(){
+  AddElements();
+  
+  function AddElements () {
+    const className = 'item';
+    const Items = document.getElementById("items");
+    for(let idx = 1; idx < 26; idx++){
+      let classSpec = `item${idx}`;
+      let div = "<div class='" + className;
+      div += ` ${classSpec}'>`
+      if(idx < 10) {
+        div += `0${idx}`
+      } else {
+        div += `${idx}`
+      }
+      div += "</div>"
+      Items.innerHTML += div;
+    }
+  }
+  
   
 }());
