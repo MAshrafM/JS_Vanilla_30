5531edb691790909a6fa15c8b47f2f73df7f0c89
MAshraf <ee.mashraf@gmail.com>
2017-12-28 19:08:57 +0200

    script

--- 27_ClickDrag/app.js
+++ 27_ClickDrag/app.js
@@ -1,11 +1,39 @@
 "use strict";
 
 (function(){
+  const slider = document.getElementById("items");
   AddElements();
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+  });
   
   function AddElements () {
     const className = 'item';
-    const Items = document.getElementById("items");
     for(let idx = 1; idx < 26; idx++){
       let classSpec = `item${idx}`;
       let div = "<div class='" + className;
@@ -16,7 +44,7 @@
         div += `${idx}`
       }
       div += "</div>"
-      Items.innerHTML += div;
+      slider.innerHTML += div;
     }
   }
   
